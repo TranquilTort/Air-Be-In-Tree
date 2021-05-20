023c34d3ad924ff8f1c5d1583f6fa80b91a6308e
Chris Regan <chrisreg007@gmail.com>
2021-05-20 13:16:52 -0400

    error handling

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,10 @@
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
-const csrf = require('csurf');
-const csrfProtection = csrf({ cookie: true });
+const apiRouter = require('./api');
+router.use('/api', apiRouter);
 
-router.get('/hello/world', csrfProtection, function(req, res) {
-    console.log(req);
+router.get('/hello/world', function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
   res.send('Hello World!');
 });
