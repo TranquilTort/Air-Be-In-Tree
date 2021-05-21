30a75ffbfa3cb0e6cab5e90991857b65b497ec4c
Chris Regan <chrisreg007@gmail.com>
2021-05-21 13:32:43 -0400

    error handling

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -24,25 +24,40 @@ const validateLogin = [
 ];
 
 router.post(
-    '/',validateLogin,
+    '/',
+    validateLogin,
     asyncHandler(async (req, res, next) => {
-        const { credential, password } = req.body;
-        const user = await User.login({ credential, password });
-        if (!user) {
+      const { credential, password } = req.body;
+      const user = await User.login({ credential, password });
+      if (!user) {
         const err = new Error('Login failed');
         err.status = 401;
         err.title = 'Login failed';
         err.errors = ['The provided credentials were invalid.'];
         return next(err);
-        }
+      }
 
-        await setTokenCookie(res, user);
+      await setTokenCookie(res, user);
 
-        return res.json({
+      return res.json({
         user,
-        });
+      });
     }),
-);
+  );
+
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        return res.json({
+          user: user.toSafeObject()
+        });
+      } else return res.json({});
+    }
+  );
+
 
 router.delete(
     '/',
