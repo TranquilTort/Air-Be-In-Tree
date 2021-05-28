c102e6b3cbcf8be2c969f20b215077a79260a40c
Chris Regan <chrisreg007@gmail.com>
2021-05-28 10:31:05 -0400

    star display on reviews

--- frontend/src/components/ReviewDisplay/index.js
+++ frontend/src/components/ReviewDisplay/index.js
@@ -9,7 +9,12 @@ function ReviewDisplay({review}){
             {review.body}
         </div>
         <div>
-            {review.stars} Stars
+            {review.stars>0 ?<span className="fa fa-star checked" ></span> : <span className="fa fa-star" ></span>}
+            {review.stars>1 ?<span className="fa fa-star checked" ></span> : <span className="fa fa-star" ></span>}
+            {review.stars>2 ?<span className="fa fa-star checked" ></span> : <span className="fa fa-star" ></span>}
+            {review.stars>3 ?<span className="fa fa-star checked" ></span> : <span className="fa fa-star" ></span>}
+            {review.stars>4 ?<span className="fa fa-star checked" ></span> : <span className="fa fa-star" ></span>}
+
         </div>
         </div>
     )
